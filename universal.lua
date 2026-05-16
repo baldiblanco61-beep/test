7b474813aea8dcdd9e5779f15f9bd919bc5bf69c
Tobi <68130707+tobi437a@users.noreply.github.com>
2026-05-16 14:41:54 +0200

    Delete universal.lua

--- universal.lua
+++
@@ -1,5 +0,0 @@
-if game.PlaceId == 142823291 then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/4ddfbd3c2dd2b3d0c0f1cd38.lua'))()
-else
-  print("hi")
-end

